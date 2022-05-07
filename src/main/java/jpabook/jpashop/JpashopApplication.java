276a3f24b312094cfc09f9816c9a03a4e64aba68
LHY1990 <gnsdud3163@gmail.com>
2022-05-07 22:46:25 +0900

    Fix : 기존 의존성 및 jdbc세팅 완료.

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopApplication {
-
+//	진입점
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
