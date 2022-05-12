7780929e090a89eeced177de5a7a0fd4baef63c3
ntripleSA <gns3163@ntriple.com>
2022-05-12 10:02:25 +0900

    Featt : 회사에서 추가 작성함

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository
+
+    @Test
+    public void 회원가입() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+
+    }
+
+    @Test
+    public void 중복회원관리리() hrows Exception{
+        //given
+
+        //when
+
+        //then
+
+
+    }
+
+}
