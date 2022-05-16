7cec26d4da4650802d4eb33593662bf29406a86a
DESKTOP-C9TU9K9\user <gnsdud3163@gmail.com>
2022-05-16 10:13:26 +0900

    Test : 테스트 코드 이어서 작성됌

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
 
@@ -16,8 +17,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceTest {
 
     @Autowired MemberService memberService;
-    @Autowired MemberRepository memberRepository
+    @Autowired
+    MemberRepository memberRepository;
 
+
+    //@Rollback(false) @Test의 경우 @Transactional이 작업 후 rollback 시켜버린다.
+    //그런경우 jpa가 디비에 트랜젝션을 보내지도 않는다. 이 경우 해당 EntityManager를 호출해서
+    // .flush()를 날리면 쿼리가 출력된다.
     @Test
     public void 회원가입() throws Exception{
         //given
@@ -32,15 +38,29 @@ class MemberServiceTest {
 
     }
 
-    @Test
-    public void 중복회원관리리() hrows Exception{
+    @Test //(expected = IllegalStateException.class) https://www.inflearn.com/questions/42746
+    public void 중복_회원_예외() throws Exception{
         //given
-
+        Member member1 = new Member();
+        member1.setName("kim1");
+        Member member2 = new Member();
+        member2.setName("kim1");
         //when
-
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            return;
+        }
         //then
+//        fail("예외가 발생해야한다."); assertion
+
+        IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> memberService.join(member2))
 
 
     }
+
+
 
 }
